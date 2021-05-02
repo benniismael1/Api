3a4c498ff7f87f3839aa85d4f88488a55d4c248b
benniismael1 <83157018+benniismael1@users.noreply.github.com>
2021-05-02 21:54:40 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["beniismael", "manogay"];
+const listkey = ["bacot", "manogay"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
